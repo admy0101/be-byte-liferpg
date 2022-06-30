f893445aede0526723ae864bfbebca84e2b08295
xViolaine <linda.guester@yahoo.de>
2022-06-30 12:55:44 +0100

    auth things

--- life_rpg/bend_life_rpg/serializers.py
+++ life_rpg/bend_life_rpg/serializers.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
 from bend_life_rpg.models import Player, Item, Task
+from django.contrib.auth import authenticate
 from django.db import models
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    email = models.EmailField(("email address"))
 
     class Meta:
         model = Player
-        fields = ['id', 'username', 'email', 'experience', 'password']
+        fields = ['id', 'username', 'email', 'currency', 'experience', 'password', 'avatar']
 
     def create(self, data):
         password = data.pop('password', None)
@@ -35,3 +35,33 @@ class TaskSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Task
         fields = ['id', 'task', 'task_difficulty', 'is_complete']
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(label="Username", write_only=True)
+
+    password = serializers.CharField(label="Password", style={'input_type': 'password'}, trim_whitespace=False, write_only=True)
+
+    def validate(self, attributes):
+        username = attributes.get('username')
+        password = attributes.get('password')
+
+        if username and password:
+            user = authenticate(request=self.context.get('request'), username=username, password=password)
+
+            if not user:
+                msg = 'Wrong Username or Password'
+                raise serializers.ValidationError(msg, code='authorization')
+
+        else:
+            msg = 'Both "Username" and "Password" are required.'
+            raise serializers.ValidationError(msg, code='authorization')
+
+        
+        attributes['user'] = user
+        return attributes
+
+    
+class CurrentUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = ['username', 'email', 'currency', 'experience', 'avatar']

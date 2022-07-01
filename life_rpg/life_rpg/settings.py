3380e30061aba53cab132053f6ea1f58b731a8c6
alousha <itsaliabdallah@gmail.com>
2022-07-01 14:56:49 +0100

    heroku changes

--- life_rpg/life_rpg/settings.py
+++ life_rpg/life_rpg/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +79,9 @@ WSGI_APPLICATION = 'life_rpg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'life_rpg',
+
     }
 }
 
@@ -136,3 +139,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())

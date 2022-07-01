892b154ef0914e9d4927ed98820a409bc9ecc7f7
alousha <itsaliabdallah@gmail.com>
2022-07-01 15:34:09 +0100

    heroku changes

--- life_rpg/bend_life_rpg/apps.py
+++ life_rpg/bend_life_rpg/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BendLifeRpgConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bend_life_rpg'
+    name = 'life_rpg.bend_life_rpg'

4c1a534290ae1b02a9aec66c7b4c4cc67685e397
alousha <itsaliabdallah@gmail.com>
2022-07-01 15:32:34 +0100

    heroku changes

--- life_rpg/life_rpg/settings.py
+++ life_rpg/life_rpg/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bend_life_rpg',
+    'life_rpg.bend_life_rpg',
     'rest_framework',
 ]
 

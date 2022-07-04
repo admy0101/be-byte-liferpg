a22f20d776b49f3678d2e2bdbe892dfe4d39d03e
alousha <itsaliabdallah@gmail.com>
2022-07-04 10:22:00 +0100

    heroku changes

--- bend_life_rpg/migrations/0001_initial.py
+++ bend_life_rpg/migrations/0001_initial.py
@@ -11,10 +11,6 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-        ('auth', '0013_alter_user_email'),
-    ]
-
     operations = [
         migrations.CreateModel(
             name='Item',
